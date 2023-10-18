2a326c6b3c9acb4b8a472e95c01575762d1dc8f4
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2023-10-17 20:20:41 -0400

    schema: update to the new spec
    I believe this new schema fits the problem description

--- app/schema.js
+++ app/schema.js
@@ -3,22 +3,50 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import gql from 'graphql-tag'
+import { buildSchema } from 'graphql'
 
-const userSchema = gql`
+const transactionSchema = buildSchema(`
   type Query {
-    user(id: Int!): User
-    allUsers: [User]
+    transaction(id: ID!): Transaction
+    allTransactions: [Transaction]
+    allTransactionsByMethod(methodName: String!): [Transaction]
+    balance: Int
   }
 
-  type User {
-    age: Int!
-    email: String!
-    hobbies: [String!]
-    id: Int!
-    name: String!
-    friends: [Int!]
+  enum Status {
+    Pending
+    Posted
   }
-`
+  
+  type Mutation {
+    addTransaction(
+      date: String!, 
+      amount: Int!, 
+      status: String!, 
+      counterPartyName: String!,
+      methodCode: String,
+      note: String
+    ): ID
+    updateTransaction(id: ID!, amount: Int, note: String): Boolean
+    deleteTransaction(id: ID!): Boolean
+  }
 
-export default userSchema
+  type Transaction {
+    id: ID!
+    date: String!
+    # Amount in cents, to avoid fractional values (21 million dollars
+    # ought to be enough for anyone)
+    amount: Int!
+    status: Status!
+    counterPartyName: String!
+    methodName: String!
+    # Even though the codes are numbers, they are not used for their
+    # numerical value. Just like you don't want phone numbers to be
+    # rounded or added or any other numerical things, we want these
+    # codes to be treated stringily.
+    methodCode: String
+    note: String
+  }
+`)
+
+export default transactionSchema
