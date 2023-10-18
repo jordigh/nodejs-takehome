6187fda43334e6a48d846d865efb0e8a8f73c245
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2023-10-17 20:29:16 -0400

    make-requests: update for the new API
    This hits all of the endpoints from spec.
    
    It's just the happy path, but I did code in for some failure states.

--- app/make-requests.sh
+++ app/make-requests.sh
@@ -7,12 +7,47 @@ echo "Starting request loop"
 while true; do
 
   # Get all users
-  echo "Fetching all users"
-  http localhost:4000 query='{allUsers{age, email, hobbies, id, name, friends}}' | jq -C
+  echo "Fetching all transactions"
+  http localhost:4000 query='{allTransactions{id, amount, note, methodCode, methodName, status, date, counterPartyName}}' | jq -C
+  sleep 2
 
-  for id in {1..10}; do
-    echo "Fetching user #${id}"
-    http localhost:4000 query="{user(id:${id}){age, email, hobbies, id, name, friends}}" | jq -C
+
+  for method in Incoming Outgoing ACH 'Card Purchase' Fee Wire; do
+      echo "Fetch transactions by $method"
+      http localhost:4000 query='{allTransactionsByMethod(methodName: "'"$method"'"){id, amount, note, methodCode, methodName, status, date, counterPartyName}}' | jq -C
+      sleep 2
   done
-  sleep 5
+
+  echo "Get the balance"
+  http localhost:4000 query='{balance}' | jq -C
+  sleep 2
+
+  echo "Insert a transaction"
+  http localhost:4000 query='mutation {addTransaction(date: "2023-10-17", amount: 510, status: "Posted", counterPartyName: "Mr Rogers")}' | tee json | jq -C
+  txn_id=$(jq .data.addTransaction json)
+  sleep 2
+
+  echo "Get the new transaction"
+  http localhost:4000 query="{transaction(id: $txn_id){id, amount, note, methodCode, methodName, status, date, counterPartyName}}" | jq -C
+  sleep 2
+
+  echo "Get the new balance"
+  http localhost:4000 query='{balance}' | jq -C
+  sleep 2
+
+  echo "Modify the transaction"
+  http localhost:4000 query='mutation {updateTransaction(id: '$txn_id', amount: 5100, note: "Mr Rogers actually made a generous donation")}' | jq -C
+  sleep 2
+
+  echo "Get the new balance again"
+  http localhost:4000 query='{balance}' | jq -C
+  sleep 2
+
+  echo "Delete the transaction"
+  http localhost:4000 query="mutation {deleteTransaction(id: $txn_id)}" | jq -C
+  sleep 2
+
+  echo "Try deleting the transaction again"
+  http localhost:4000 query="mutation {deleteTransaction(id: $txn_id)}" | jq -C
+  sleep 2
 done
