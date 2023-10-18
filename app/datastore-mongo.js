938b7705880e1cde16f92dc8e1c7c676673feac4
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2023-10-17 20:28:37 -0400

    mongo: implement this datastore
    pretty straightforward

--- app/datastore-mongo.js
+++ app/datastore-mongo.js
@@ -3,38 +3,116 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { MongoClient } from 'mongodb'
-import { generateUsers } from './utils.js'
+import { MongoClient, ObjectId } from 'mongodb'
+import { generateTransactions, methodCodes } from './utils.js'
+
+async function getCollection() {
+  const client = new MongoClient(process.env.MONGO_URL)
+  await client.connect()
+  const db = client.db('transactions')
+  const txns = db.collection('transactions')
+  return { client, txns }
+}
 
 async function initData() {
-  const client = new MongoClient(process.env.MONGO_URL)
+  const { client, txns } = await getCollection()
   try {
-    await client.connect()
-    const allUsers = generateUsers(10)
-    const db = client.db('users')
-    const friends = db.collection('friends')
-    await friends.deleteMany()
-
-    const result = await friends.insertMany(allUsers, { ordered: true })
-    console.log(`${result.insertedCount} users were created`)
+    const allTransactions = generateTransactions(50)
+    await txns.deleteMany()
+    await txns.insertMany(allTransactions)
   } finally {
     client.close()
   }
 }
 
-async function getUserById(id) {
-  console.log(`Fetch user #${id}`)
-  const client = new MongoClient(process.env.MONGO_URL)
+async function getTransactionById(id) {
+  const { client, txns } = await getCollection()
   try {
-    await client.connect()
-    const db = client.db('users')
-    const friends = db.collection('friends')
-    const result = await friends.findOne({ _id: id })
-    console.log(result)
-    return result
+    id = new ObjectId(id)
+    return await txns.findOne({ _id: id })
   } finally {
     client.close()
   }
 }
 
-export { getUserById, initData }
+async function getAllTransactions(methodName) {
+  const { client, txns } = await getCollection()
+  try {
+    let query
+    if (!methodName) {
+      query = {}
+    } else if (methodName === 'Incoming') {
+      query = { amount: { $gte: 0 }, methodCode: null }
+    } else if (methodName === 'Outgoing') {
+      query = { amount: { $lt: 0 }, methodCode: null }
+    } else {
+      query = { methodCode: methodCodes[methodName] }
+    }
+    return await txns.find(query).toArray()
+  } finally {
+    client.close()
+  }
+}
+
+async function getBalance() {
+  const { client, txns } = await getCollection()
+  try {
+    const groups = await txns
+      .aggregate([{ $group: { _id: 1, total: { $sum: '$amount' } } }])
+      .toArray()
+    return groups[0].total
+  } finally {
+    client.close()
+  }
+}
+
+async function addTransaction(transaction) {
+  const { client, txns } = await getCollection()
+  try {
+    const result = await txns.insertOne(transaction)
+    if (result.acknowledged) {
+      return result.insertedId
+    }
+    return null
+  } finally {
+    client.close()
+  }
+}
+
+async function updateTransaction(query) {
+  const { client, txns } = await getCollection()
+  try {
+    const filter = { _id: new ObjectId(query._id) }
+    const values = {}
+    for (const [key, value] of Object.entries(query)) {
+      if (key !== '_id' && value !== undefined) {
+        values[key] = value
+      }
+    }
+    const result = await txns.updateOne(filter, { $set: values })
+    return result.modifiedCount === 1
+  } finally {
+    client.close()
+  }
+}
+
+async function deleteTransaction(id) {
+  const { client, txns } = await getCollection()
+  try {
+    id = new ObjectId(id)
+    const result = await txns.deleteOne({ _id: id })
+    return result.deletedCount === 1
+  } finally {
+    client.close()
+  }
+}
+
+export {
+  initData,
+  getTransactionById,
+  getAllTransactions,
+  getBalance,
+  addTransaction,
+  updateTransaction,
+  deleteTransaction
+}
